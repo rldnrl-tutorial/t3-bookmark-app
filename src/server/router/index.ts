865e3a0a8a01c4b4bed14ab54698b251aa04649a
rldnrl <devpark1002@gmail.com>
2022-08-09 21:02:54 +0900

    feat: link router 등록

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,8 +1,11 @@
 // src/server/router/index.ts
 import { createRouter } from './context'
 import superjson from 'superjson'
+import { linkRouter } from './link.router'
 
-export const appRouter = createRouter().transformer(superjson)
+export const appRouter = createRouter()
+  .transformer(superjson)
+  .merge('link.', linkRouter)
 
 // export type definition of API
 export type AppRouter = typeof appRouter
