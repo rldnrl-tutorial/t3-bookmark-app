eee1b42e56c107aeeb84b8c32eb4fc74e61eb533
rldnrl <devpark1002@gmail.com>
2022-08-10 20:18:11 +0900

    feat: set up auth0 with trpc

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -2,14 +2,23 @@
 import * as trpc from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
 import { prisma } from '../db/client'
+import { getSession } from '@auth0/nextjs-auth0'
 
-export const createContext = (opts?: trpcNext.CreateNextContextOptions) => {
-  const req = opts?.req
-  const res = opts?.res
+export const createContext = (opts: trpcNext.CreateNextContextOptions) => {
+  const req = opts.req
+  const res = opts.res
+
+  const session = getSession(req, res)
+
+  if (!session) return { req, res, prisma }
+
+  const { user, accessToken } = session
 
   return {
     req,
     res,
+    user,
+    accessToken,
     prisma,
   }
 }
