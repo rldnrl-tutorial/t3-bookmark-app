5e620fc22385e99b2bf1f5ec78729d49c456b8ba
rldnrl <devpark1002@gmail.com>
2022-08-11 21:51:45 +0900

    feat: set up next-auth api

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,30 @@
-import { handleAuth } from '@auth0/nextjs-auth0'
+import NextAuth, { type NextAuthOptions } from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
 
-export default handleAuth()
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { prisma } from '../../../server/db/client'
+import { env } from '../../../env/server.mjs'
+
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id
+      }
+      return session
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  secret: env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/auth/signin',
+  },
+}
+
+export default NextAuth(authOptions)
