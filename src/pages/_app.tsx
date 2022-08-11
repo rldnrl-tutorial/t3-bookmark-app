24b68ff180da6b6e9338434ad9f556350eed038e
rldnrl <devpark1002@gmail.com>
2022-08-11 21:50:18 +0900

    feat: change next-auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,22 @@
 // src/pages/_app.tsx
 import { withTRPC } from '@trpc/next'
-import type { AppRouter } from '../server/router'
 import type { AppType } from 'next/dist/shared/lib/utils'
 import superjson from 'superjson'
+import { SessionProvider } from 'next-auth/react'
+import type { AppRouter } from '../server/router'
 import '../styles/globals.css'
 import Layout from '../libs/ui/Layout'
-import { UserProvider } from '@auth0/nextjs-auth0'
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp: AppType = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
   return (
-    <UserProvider>
+    <SessionProvider session={session}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </UserProvider>
+    </SessionProvider>
   )
 }
 
