4ac03d16fa4df0d8b364facf5a28287b734bc1e9
rldnrl <devpark1002@gmail.com>
2022-08-11 22:08:53 +0900

    feat: apply next auth

--- src/libs/ui/Header/Header.tsx
+++ src/libs/ui/Header/Header.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
-import { useUser } from '@auth0/nextjs-auth0'
+import { useSession } from 'next-auth/react'
 import Avatar from '../Avatar'
 
 const Header = () => {
-  const { user } = useUser()
+  const { data: session, status } = useSession()
 
   return (
     <header className="text-gray-600 body-font">
@@ -27,14 +27,14 @@ const Header = () => {
           </a>
         </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          {user ? (
+          {session ? (
             <div className="flex items-center space-x-5">
               <Link href="/api/auth/logout">
                 <a className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
                   Logout
                 </a>
               </Link>
-              <Avatar src={user.picture} />
+              <Avatar src={session.user?.image} />
             </div>
           ) : (
             <Link href="/api/auth/login">
