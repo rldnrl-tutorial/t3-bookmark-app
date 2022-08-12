85557b57e014ecbdbabaf04e687348833dda2252
rldnrl <devpark1002@gmail.com>
2022-08-12 22:21:22 +0900

    feat: apply next-auth in trpc-context

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -1,24 +1,24 @@
 // src/server/router/context.ts
 import * as trpc from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
-import { prisma } from '../db/client'
-import { getSession } from '@auth0/nextjs-auth0'
+import { unstable_getServerSession as getServerSession } from 'next-auth'
 
-export const createContext = (opts: trpcNext.CreateNextContextOptions) => {
+import { authOptions as nextAuthOptions } from '../../pages/api/auth/[...nextauth]'
+import { prisma } from '../db/client'
+
+export const createContext = async (
+  opts: trpcNext.CreateNextContextOptions
+) => {
   const req = opts.req
   const res = opts.res
 
-  const session = getSession(req, res)
-
-  if (!session) return { req, res, prisma }
-
-  const { user, accessToken } = session
+  const session =
+    req && res && (await getServerSession(req, res, nextAuthOptions))
 
   return {
     req,
     res,
-    user,
-    accessToken,
+    session,
     prisma,
   }
 }
