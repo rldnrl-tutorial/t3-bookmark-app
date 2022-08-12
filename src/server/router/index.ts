fc9c8343b5a5e58b7e6fae3b9363d7994e2be5ac
rldnrl <devpark1002@gmail.com>
2022-08-12 22:24:31 +0900

    feat: add link router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,11 +1,12 @@
 // src/server/router/index.ts
 import { createRouter } from './context'
 import superjson from 'superjson'
-import { linkRouter } from './link.router'
+import { linkRouter, protectedLinkRouter } from './link.router'
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge('link.', linkRouter)
+  .merge('protected-link.', protectedLinkRouter)
 
 // export type definition of API
 export type AppRouter = typeof appRouter
