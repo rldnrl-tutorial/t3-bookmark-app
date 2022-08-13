83a500594252a85f462043b97bfbbb49c84bc86f
rldnrl <devpark1002@gmail.com>
2022-08-13 13:33:00 +0900

    feat: export Card Component

--- src/libs/ui/Card/index.tsx
+++ src/libs/ui/Card/index.tsx
@@ -1,8 +1,10 @@
 import Card from './Card'
 
 export { default as CardImage } from './CardImage'
+export { default as CardBody } from './CardBody'
 export { default as CardTitle } from './CardTitle'
-export { default as CardDetail } from './CardDetail'
+export { default as CardDetail } from './CardDescription'
 export { default as CardCategory } from './CardCategory'
+export { default as CardLink } from './CardLink'
 
 export default Card
