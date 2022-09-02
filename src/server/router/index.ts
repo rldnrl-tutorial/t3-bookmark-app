d76fab15a6eb8d9700dcea90a758889aa55a85ca
rldnrl <devpark1002@gmail.com>
2022-09-02 22:25:34 +0900

    feat: login으로 변경된 부분 적용

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,13 +2,13 @@
 import { createRouter } from './context'
 import superjson from 'superjson'
 import { linkRouter, protectedLinkRouter } from './link.router'
-import { signinRouter } from './signin.router'
+import { loginRouter } from './login.router'
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge('link.', linkRouter)
   .merge('protected-link.', protectedLinkRouter)
-  .merge('sign-in.', signinRouter)
+  .merge('login.', loginRouter)
 
 // export type definition of API
 export type AppRouter = typeof appRouter
